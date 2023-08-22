92071e015f22d48e186d19977420b1bd179a4da5
HM <hieumdd@gmail.com>
2023-08-22 23:19:55 +0700

    feat: Update table name

--- cloud-functions/src/pipeline/pipeline.service.ts
+++ cloud-functions/src/pipeline/pipeline.service.ts
@@ -6,16 +6,16 @@ export const loadFromGCS = async (filename: string) => {
     logger.info({ action: 'load-from-gcs', filename });
 
     const loadOptions = {
-        table: 'Report',
+        table: 'TotalSales',
         schema: [
-            {name: 'id', type: 'NUMERIC'},
-            {name: 'customer', type: 'STRING'},
-            {name: 'lead_start_date', type: 'DATE'},
-            {name: 'project_status', type: 'STRING'},
-            {name: 'revenue', type: 'NUMERIC'},
-            {name: 'sales_vendor', type: 'STRING'},
-            {name: 'setter', type: 'STRING'},
-            {name: 'system_size', type: 'NUMERIC'},
+            { name: 'id', type: 'NUMERIC' },
+            { name: 'customer', type: 'STRING' },
+            { name: 'lead_start_date', type: 'DATE' },
+            { name: 'project_status', type: 'STRING' },
+            { name: 'revenue', type: 'NUMERIC' },
+            { name: 'sales_vendor', type: 'STRING' },
+            { name: 'setter', type: 'STRING' },
+            { name: 'system_size', type: 'NUMERIC' },
         ],
     };
 
